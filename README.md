6a49cf3beac47d62de3a31a0a23cc2f5b872ea3f
Aggelos Tselios <aggu8914@gmail.com>
2023-11-30 00:04:32 +0200

    Fibonacci done.

---
+++ README.md
@@ -0,0 +1,15 @@
+1)Clone the repository with this command:
+
+'git clone https://github.com/AggelosTse/Fibonacci-.git'
+
+2)navigate into the project's directory:
+
+cd Fibonacci-
+
+3)Compile it with this command:
+
+g++ main.cpp fib.cpp -o hello
+
+4)Run it: if you have linux or macOS, write this command: ./hello
+
+if you have windows, write this command: hello.exe
--- fib.cpp
+++ fib.cpp
@@ -7,6 +7,17 @@ using std::cout,std::cin;
 fibonacci::fibonacci(int x, int y) {ar1 = x, ar2 = y;}
 fibonacci::~fibonacci() {}
 
+
+void fibonacci::clearscreen()   //clears old dta from the screen.
+{
+  #ifdef _WIN32     //checks if the program is running from a Windows operating system.
+    system("cls");  //if it does, it runs this command, which clears old data from the screen (runs only on windows).
+  #else
+    system("clear");  //if it is running from unix or macOS, it runs this command that does the same thing.
+  #endif
+}
+
+
 void fibonacci::Fib(int ar1,int ar2, int posoiarithmoi, int pl)     //recursive function that loops and prints fibonacci sequence.
     {
         int s = ar1 + ar2;
@@ -19,8 +30,10 @@ void fibonacci::Fib(int ar1,int ar2, int posoiarithmoi, int pl)     //recursive 
         return;
     }
 
+
 void fibonacci::userinput()    //Gets user's input and checks if its valid.
     {
+        clearscreen();
         cout << "How many Fibonacci numbers do you want to print?\n";
         int input;
         cin >> input;
@@ -31,6 +44,7 @@ void fibonacci::userinput()    //Gets user's input and checks if its valid.
              cout << "Invalid Input, try again." << "\n";
              cin >> input;
             }    
+            clearscreen();
         if(input == 1) {cout << ar1 << ".\n";}                      //if 1 is pressed, it prints the first number of the sequence.
         else if(input == 2) {cout << ar1 << "," << ar2 << ".\n";}   //if 2 is pressed, it prints the first and the second number.
         else 
